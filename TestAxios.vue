3a504b7587169fa5268562022cf47fda3c79167d
qxj-gd <57125731+qxj-gd@users.noreply.github.com>
2021-05-26 19:15:35 +0800

    Add files via upload

---
+++ TestAxios.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  mounted() {
+    axios.get("/test.json").then((res) => {
+      console.log(res.data);
+    });
+  },
+};
+</script>
